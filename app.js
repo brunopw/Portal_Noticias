7c2b01c49ee9e6f0a3dda526375f1cba0b581e74
brunopw <bruno_p_w@hotmail.com>
2017-07-18 20:38:08 -0300

    Commit all files in new repository.

---
+++ app.js
@@ -0,0 +1,11 @@
+var app = require('./config/server');
+
+//Routes Manual
+//var rotaHome = require('./app/routes/home')(app);
+//var rotaNoticias = require('./app/routes/noticias')(app);
+//var rotaFormAddNoticia = require('./app/routes/form_add_noticia')(app);
+
+//Start Server
+app.listen(3000, function() {
+    console.log("Servidor Online!");
+});
